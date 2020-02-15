6cb5c756a6f29391e3b426fec87239bd413ea2ed
Ogunboyejo Olatunde <josiaholatunde17@gmail.com>
2020-02-15 17:39:25 +0100

    feature: Added the tabbed component for the questions list page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,37 +1,56 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { loginUser } from '../actions/usersActions'
+const Navbar = ({ loggedInUser }) => {
 
-const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-danger text-white sticky-top" >
-           <div className='container'>
-           <Link className="navbar-brand" to="/">Would You Rather</Link>
+            <div className='container'>
+                <Link className="navbar-brand" to="/">Would You Rather</Link>
 
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul className="navbar-nav mr-auto">
-                    <li className="nav-item active">
-                        <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/question/add'>New Question</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to='/leaderboard'>Leaderboard
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/question/add'>New Question</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to='/leaderboard'>Leaderboard
                         </Link>
-                    </li>
-                </ul>
-                <ul className='navbar-nav nav-right'>
-                    <li className="nav-item d-flex align-items-center">
-                        Hello Olatunde
-                    </li>
-                    <li className="nav-item ml-3">
-                        <Link className="nav-link" to='/logout' >Logout</Link>
-                    </li>
-                </ul>
+                        </li>
+                    </ul>
+                    <ul className='navbar-nav nav-right'>
+                        { loggedInUser ? (
+                            <Fragment>
+                                <li className="nav-item d-flex align-items-center">
+                                   Hello {loggedInUser.name}
+                                   <div style={{ height: '2rem', width: '2rem'}} className='ml-2'>
+                                   <img src={loggedInUser.avatarURL} className='img-rounded img-fluid'  />
+                                   </div>
+                                </li>
+                                <li className="nav-item ml-3">
+                                    <Link className="nav-link" to='/logout' >Logout</Link>
+                                </li>
+                            </Fragment>
+                        ) : (
+                                <li className="nav-item d-flex align-items-center">
+                                    <Link className="nav-link" to='/login' >Login</Link>
+                                </li>
+                            )}
+                    </ul>
+                </div>
             </div>
-           </div>
         </nav>
     )
 }
 
-export default Navbar
+const mapStateToProps = ({ authedUser, users }) => {
+    return {
+        loggedInUser: authedUser ? users[authedUser] && users[authedUser] : null
+    }
+}
+
+export default connect(mapStateToProps)(Navbar)
--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react'
-
+import { Tab, Tabs } from 'react-bootstrap'
 export class Questions extends Component {
+    state = {
+        key: 'unanswered'
+    }
+    setCurrentTab = key => {
+        this.setState({ key })
+    }
     render() {
+        const { key } = this.state
         return (
-            <div>
-                Questions List
+            <div className='mt-5'>
+                <Tabs id='questions-tab' bsClass='text-black' activeKey={key} onSelect={this.setCurrentTab}>
+                    <Tab eventKey='unanswered' title='Unanswered Questions' >
+                        Yo
+                </Tab>
+                    <Tab eventKey='answered' title='Answered Questions' >
+                        Ya
+                </Tab>
+                </Tabs>
             </div>
         )
     }
